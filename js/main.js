d3bb182427184d2beefc72a7319b374ce3a7ff44
Andersen Yang <andersenyang@ayang.local>
2015-09-06 20:13:49 -0700

    Hide level details by default

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 $(function () {
     var template = _.template($("#level-template").html());
-
-    $.getJSON('js/levels.json', function (data) {
-	_.each(data.levels, function (level) {
+    var renderLevelsFromData = function (levels) {
+	_.each(levels, function (level) {
 	    $(".level-container").append(template(level));
 	});
+
+	$(".level").click(function () {
+	    var $level = $(this);
+	    var $details = $level.find(".level-details");
+	    if ($details.is(":visible")) {
+		$level.removeClass("active");
+		$details.slideUp();
+	    } else {
+		$level.addClass("active");
+		$details.slideDown();
+	    }
+	});
+    }
+
+    $.getJSON('js/levels.json', function (data) {
+	renderLevelsFromData(data.levels);
     });
 });
